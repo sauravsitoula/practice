7a53c8ff48cb05e6f7233a498c24cbf38cad18b8
madhavadhikari <madhava492@gmail.com>
2018-04-10 18:19:48 +0545

    ddd

--- src/main/java/com/texas/practice/model/User.java
+++ src/main/java/com/texas/practice/model/User.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
 @Table(name= "userTable")
-public class userModel implements Serializable{
+public class User implements Serializable{
 	@Column(name= "id" )
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
